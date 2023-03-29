49d96bc93f79ea56eca9630aa364e4c09ce6eaa0
robertvlad <vladrob96@gmail.com>
2023-03-30 01:49:48 +0200

    aggiunto adEventListener al click della box

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ for (let i = 0; i < 100; i++) {
 
     container.appendChild(box);
 };
+
+let boxes = document.getElementsByClassName('box');
+console.log(boxes);
+
+for (let i = 0; i < boxes.length; i++) {
+    
+    let single_box = boxes[i];
+
+    single_box.addEventListener('click', function() {
+
+        if (this.classList.contains('red')) {
+            this.classList.remove('red')
+        }
+        else {
+            this.classList.add('red')
+        }
+    })
+}
